06497ad362713ef2b023ae99bc82ca79e8534e2f
rruiz1982 <ruivia@hotmail.com>
2023-08-15 20:07:36 -0300

    Actualizado

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import { useState } from 'react'
 import './App.css'
+import Card from './components/Cards'
 
 function App() {
 
 
   return (
-    <>
+    <div className='App'>
       
-   
-    </>
+    <h1>Carga de estudiantes</h1>
+    <form></form>
+
+    <Card />
+  </div>
   )
 }
 
